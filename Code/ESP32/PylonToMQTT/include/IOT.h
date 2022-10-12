ef6d01c7c8b520ae93c34d9e281221061c35d7d2
Graham <graham.a.ross@gmail.com>
2022-10-12 17:42:14 -0400

    Update IOT.h

--- Code/ESP32/PylonToMQTT/include/IOT.h
+++ Code/ESP32/PylonToMQTT/include/IOT.h
@@ -15,7 +15,7 @@ extern "C"
 #include <IotWebConfESP32HTTPUpdateServer.h>
 #include "IOTCallbackInterface.h"
 
-#define CONFIG_VERSION "V1.1.0" // major.minor.build (major or minor will invalidate the configuration)
+#define CONFIG_VERSION "V1.1.2" // major.minor.build (major or minor will invalidate the configuration)
 #define HOME_ASSISTANT_PREFIX "homeassistant" // MQTT prefix used in autodiscovery
 #define STR_LEN 255                            // general string buffer size
 #define CONFIG_LEN 32                         // configuration string buffer size
